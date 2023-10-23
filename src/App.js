a73c05998202432d8a89c8ee58085c19e8cb5f43
sandhya <79160128+sandhyabaiju86@users.noreply.github.com>
2023-10-23 14:22:54 +0400

    changed the files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className='App-header'>
       <h1>Hello Everyone</h1>
-      <Weather  cities="Kannur"/>
+      <Weather cities="Kannur"/>
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -2,8 +2,14 @@ import React,{useState} from "react";
 import axios from "axios";
 import { FallingLines } from  'react-loader-spinner';
 
-export default function Weather(props){
+export default function Weather(){
     let[weather,setWeather] = useState("");
+    let apiKey = "9eca7aac0b071aa16e3cb063adba0785";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kannur&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(showWeather);
+
+
      function showWeather(response){
        
        setWeather({temperature: response.data.main.temp,
@@ -11,22 +17,20 @@ export default function Weather(props){
 
         
     }
-    let apiKey = "9eca7aac0b071aa16e3cb063adba0785";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.cities}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(showWeather);
-    if(props.cities){
+    
+    
     return(
+      <div>
         <h3>Todays Weather in {weather.city} is {Math.round(weather.temperature)} </h3>
+        <FallingLines
+          color="#4fa94d"
+          width="100"
+          visible={true}
+          ariaLabel='falling-lines-loading'
+        />
+        </div>
     )
-}
-else{
-    return(<FallingLines
-  color="#4fa94d"
-  width="100"
-  visible={true}
-  ariaLabel='falling-lines-loading'
-/>)
 
-}
+    
+
 }
