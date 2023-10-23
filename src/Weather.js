cf6af2b7f3e612844a02df1ff393261b90b041d2
sandhya <79160128+sandhyabaiju86@users.noreply.github.com>
2023-10-23 14:41:20 +0400

    added theWeaher.js and Weathr.css files

--- src/Weather.js
+++ src/Weather.js
@@ -1,36 +1,71 @@
 import React,{useState} from "react";
 import axios from "axios";
 import { FallingLines } from  'react-loader-spinner';
+import "./Weather.css"
 
-export default function Weather(){
-    let[weather,setWeather] = useState("");
+
+export default function Weather() {
+  let [city, setCity] = useState("");
+  let [message, setMessage] = useState("");
+  
+  function weatherData(event) {
+    event.preventDefault();
     let apiKey = "9eca7aac0b071aa16e3cb063adba0785";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kannur&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(showWeather);
-
-
-     function showWeather(response){
-       
-       setWeather({temperature: response.data.main.temp,
-    city:response.data.name})
-
-        
+  }
+  function showWeather(response) {
+    if (city) {
+      const temperature = response.data.main.temp;
+      const humidity = response.data.main.humidity;
+      const wind = response.data.wind.speed;
+      const description = response.data.weather[0].description;
+      const icon = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+      setMessage({
+        Temperature: temperature,
+        Humidity: humidity,
+        Wind: wind,
+        Description: description,
+        icon: icon
+      });
     }
-    
-    
-    return(
-      <div>
-        <h3>Todays Weather in {weather.city} is {Math.round(weather.temperature)} </h3>
-        <FallingLines
+  }
+
+  function handleCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+  let form = (
+    <form onSubmit={weatherData}>
+      <input type="search" onChange={handleCity} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (city) {
+    // alert(message);
+    return (
+      <div className="Weather">
+        {form}
+        <ul>
+          <li>Temperature:{Math.round(message.Temperature)} C</li>
+          <li>Description:{message.Description}</li>
+          <li>Humidity:{message.Humidity}%</li>
+          <li>Wind:{message.Wind}Km/h</li>
+          <li>
+            <img src={message.icon} alt={message.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return <div className="Weather">{form}
+    <FallingLines
           color="#4fa94d"
           width="100"
           visible={true}
           ariaLabel='falling-lines-loading'
-        />
-        </div>
-    )
-
-    
-
+        /></div>;
+  }
 }
