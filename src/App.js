3791a64da6c05f74679dea171ac8c8b3ed00e5c8
sandhya <79160128+sandhyabaiju86@users.noreply.github.com>
2023-10-23 14:43:14 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className='App-header'>
-      <h1>Hello Everyone</h1>
-      <Weather cities="Kannur"/>
+      
+      <h1>WEATHER APP</h1>
+      <Weather />
+    
       </header>
     </div>
   );
